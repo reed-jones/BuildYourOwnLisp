6d9e9532f0f48cc930e69a17d940175f396dd28c
Reed Jones <develop@reedjones.com>
2017-02-10 17:58:06 -0700

    An Interactive Prompt

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+// User input buffer. Size of 2048
+static char input[2048];
+
 int main(int argc, char** argv)
 {
-    printf("Hello, World!\n");
+//    Print Version Info
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    for(;;)
+    {
+//        Output our prompt
+        fputs("lispy> ", stdout);
+
+//        read user input
+        fgets(input, 2048, stdin);
+
+//        echo back to user
+        printf("No you're a %s", input);
+    }
     return 0;
 }
