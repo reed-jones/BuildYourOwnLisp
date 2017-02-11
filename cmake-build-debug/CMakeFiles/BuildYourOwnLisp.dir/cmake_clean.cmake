5bbd0668bdf84806dcd57a741f1d7d1b38ebba03
Reed Jones <develop@reedjones.com>
2017-02-10 17:43:11 -0700

    Initial CLIon Upload

---
+++ cmake-build-debug/CMakeFiles/BuildYourOwnLisp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BuildYourOwnLisp.dir/main.c.o"
+  "BuildYourOwnLisp.pdb"
+  "BuildYourOwnLisp.exe"
+  "libBuildYourOwnLisp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BuildYourOwnLisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
